a8ab18fbb379e332256fbf3f6e84b619a5e7d2ed
Makarov Yuriy <makarov.yuriy@pixty.com>
2016-09-04 00:29:14 +0300

    Remove unused dependencies

--- Example/Pods/Target Support Files/Pods-MYTableViewIndex_Tests/Pods-MYTableViewIndex_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-MYTableViewIndex_Tests/Pods-MYTableViewIndex_Tests-frameworks.sh
@@ -82,16 +82,3 @@ strip_invalid_archs() {
   fi
 }
 
-
-if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble-Snapshots/Nimble_Snapshots.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
-fi
-if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/FBSnapshotTestCase/FBSnapshotTestCase.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble-Snapshots/Nimble_Snapshots.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
-fi
